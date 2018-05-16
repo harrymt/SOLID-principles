d0f7df5fe4cae79f4d76f0f06fd140d5ed79f491
Harry Mumford-Turner <harrymt@me.com>
2018-05-16 15:24:07 +0100

    Added description of Interface Segregation

--- Solid/InterfaceSegregation/Isp.cs
+++ Solid/InterfaceSegregation/Isp.cs
@@ -6,5 +6,49 @@
         {
             return "Interface Segregation";
         }
+
+        // If we want to add more functionality, don't add to existing
+        // interfaces, segregate them out.
+        interface ICustomer // existing
+        {
+            void Add();
+        }
+        // BAD:
+        interface ICustomerImproved
+        {
+            void Add();
+            void Read(); // Existing Functionality, BAD
+        }
+
+        // GOOD:
+        // Just create another interface, that a class can ALSO extend from
+        interface ICustomerV1 : ICustomer
+        {
+            void Read();
+        }
+
+        class CustomerWithRead : ICustomer, ICustomerV1
+        {
+            public void Add()
+            {
+                var customer = new Customer();
+                customer.Add(new Database());
+            }
+
+            public void Read()
+            {
+                // GOOD: New functionality here!
+            }
+        }
+
+        // e.g.
+        void ManipulateCustomers()
+        {
+            var database = new Database();
+            var customer = new Customer();
+            customer.Add(database); // Old functionality, works fine
+            var readCustomer = new CustomerWithRead();
+            readCustomer.Read(); // Good! New functionalty is separate from existing customers
+        }
     }
 }
